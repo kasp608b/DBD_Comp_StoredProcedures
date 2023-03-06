6f455102ac10661eb56d7409866cde1b8afbd1f5
scupak <antonibujko@hotmail.com>
2023-03-06 09:32:30 +0100

    stuff

--- DBD_Comp_StoredProcedures/DBD_Comp_StoredProcedures/Program.cs
+++ DBD_Comp_StoredProcedures/DBD_Comp_StoredProcedures/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Configuration;
+
+Console.WriteLine(ConfigurationManager.ConnectionStrings["SqlConnectionString"].ConnectionString);
